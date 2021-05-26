ddffad0a8f21258293bca97bc87407a5578b3e64
fransoufil <francixco.sf@gmail.com>
2021-05-26 17:58:17 -0300

    "First: project created with spring.io"

---
+++ src/main/java/com/fransoufil/springmongointellijude/SpringMongoIntellijUdeApplication.java
@@ -0,0 +1,13 @@
+package com.fransoufil.springmongointellijude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoIntellijUdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoIntellijUdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fransoufil/springmongointellijude/SpringMongoIntellijUdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fransoufil.springmongointellijude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoIntellijUdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
